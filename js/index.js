49d3995357409f92a990ce0a807a1a6e8710167b
alpowell24 <alpowell94@gmail.com>
2021-05-03 07:10:34 -0700

    jquery test

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-console.log("hiiiiii");
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $('.fadein').each(function (i) {
+
+            console.log("hi");
+
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            if (bottom_of_window > bottom_of_element) {
+                $(this).animate({ 'opacity': '1' }, 1000);
+            }
+
+        });
+    });
+});
