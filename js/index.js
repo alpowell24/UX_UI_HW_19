9d53124698b326159ffc742c198d327d4f306d9a
alpowell24 <alpowell94@gmail.com>
2021-05-03 16:00:15 -0700

    add header

--- js/index.js
+++ js/index.js
@@ -1,18 +1,23 @@
 $(document).ready(function () {
-    // $(window).scroll(function () {
 
+    /* Every time the window is scrolled ... */
+    $(window).scroll(function () {
 
-    $('.fadein').each(function (i) {
+        /* Check the location of each desired element */
+        $('.fadein').each(function (i) {
 
-        var bottom_of_element = $(this).offset().top + $(this).outerHeight();
-        var bottom_of_window = $(window).scrollTop() + $(window).height();
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-        if (bottom_of_window > bottom_of_element) {
-            $(this).animate({ 'opacity': '1' }, 1000);
-        }
+            /* If the object is completely visible in the window, fade it it */
+            if (bottom_of_window > bottom_of_object) {
+
+                $(this).animate({ 'opacity': '1' }, 1500);
+
+            }
+
+        });
 
     });
 
-
-    //  });
 });
