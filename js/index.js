c22353bbe12777c2929a51426929d6fdaa3ec681
alpowell24 <alpowell94@gmail.com>
2021-05-04 07:41:23 -0700

    added interactivity for hover states

--- js/index.js
+++ js/index.js
@@ -1,23 +1,27 @@
 $(document).ready(function () {
 
-    /* Every time the window is scrolled ... */
-    $(window).scroll(function () {
+    $('.fadein').each(function (i) {
 
-        /* Check the location of each desired element */
-        $('.fadein').each(function (i) {
+        var bottom_of_object = $(this).position().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
+        if (bottom_of_window > bottom_of_object) {
 
-            /* If the object is completely visible in the window, fade it it */
-            if (bottom_of_window > bottom_of_object) {
+            $(this).animate({ 'opacity': '1' }, 1500);
 
-                $(this).animate({ 'opacity': '1' }, 1500);
-
-            }
-
-        });
+        }
 
     });
-
 });
+
+
+$("button").hover(
+    function () {
+        $(this).css("background-color", "#f88410");
+        $(this).css("font-weight", "800");
+    }, function () {
+        $(this).css("background-color", "#f9a756");
+        $(this).css("font-weight", "700");
+
+    }
+);
